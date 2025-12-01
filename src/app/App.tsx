19a3317a8b999e8a866b5cdc961e3788cdf297f4
itsslava <77506882+itsslava@users.noreply.github.com>
2025-12-01 21:57:14 +0300

    feat: добавить панель настроек и привязать её к инструментам

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import styles from './app.module.scss';
 import { Canvas } from '@widgets/canvas';
+import { SettingsBar } from '@widgets/settings-bar';
 import { ToolBar } from '@widgets/toolbar';
 
 function App() {
 	return (
 		<div className={styles.app}>
 			<ToolBar />
+			<SettingsBar />
 			<Canvas />
 		</div>
 	);
